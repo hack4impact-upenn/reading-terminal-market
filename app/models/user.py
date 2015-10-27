e0a895ec1633abe0addac48531c147604033d9da
Hunter Lightman <git.huntrr@gmail.com>
2015-10-27 12:32:24 -0400

    No longer any cases where a user is created without a role

--- app/models/user.py
+++ app/models/user.py
@@ -73,13 +73,6 @@ class User(UserMixin, db.Model):
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
-        if self.role is None:
-            if not User.query.first():
-                Role.insert_roles()
-                self.role = Role.query.filter_by(
-                    permissions=Permission.ADMINISTER).first()
-            else:
-                self.role = Role.query.filter_by(default=True).first()
 
     def full_name(self):
         return '%s %s' % (self.first_name, self.last_name)
