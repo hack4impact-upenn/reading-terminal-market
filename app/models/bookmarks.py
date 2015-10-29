25a594f4612dcd5b1e79493e3d71cc42796c80a4
kbharathala <kbharathala@gmail.com>
2015-10-29 16:17:42 -0400

    Made changes from pr

--- app/models/bookmarks.py
+++ app/models/bookmarks.py
@@ -4,13 +4,11 @@ from .. import db, login_manager
 class Bookmarks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     listing_id = db.Column(db.Integer, unique=True)
-    merchant_id = db.Column(db.Integer)
-    vendor_id = db.Column(db.Integer, db.ForeignKey('person.id')) #also needs to be added to Hunter's Vendors model
+    merchant_id = db.Column(db.Integer, db.ForeignKey('user.id')) #also needs to be added to Hunter's Vendors model
 
-    def __init__(self, listing_id, merchant_id, vendor_id):
+    def __init__(self, listing_id, merchant_id):
         self.listing_id = listing_id
         self.merchant_id = merchant_id
-        self.vendor_id = vendor_id
 
     def __repr__(self):
-        return '<User: %d Bookmarked Object: %d from Vendor: %d>' % self.vendor_id, self.listing_id, self.merchant_id
+        return "<User: {} Bookmarked Listing: {}".format(self.merchant_id, self.listing_id)
