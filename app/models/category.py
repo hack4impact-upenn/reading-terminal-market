f3d74736058e8352e24b710dd38299dcb0b3b0fc
ayushgoy <ayush.goy01@gmail.com>
2015-11-03 19:17:56 -0500

    added unit removed parent

--- app/models/category.py
+++ app/models/category.py
@@ -6,5 +6,4 @@ class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     parent_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
-    parent = db.relationship("Categories", remote_side=[id])
-
+    unit = db.String(64)
