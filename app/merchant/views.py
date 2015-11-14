512ed4931710a6372cb7d6abd150b194aca3179c
Hunter Lightman <git.huntrr@gmail.com>
2015-11-13 23:36:16 -0500

    Implemented a listings template that can be used on all listings pages

--- app/merchant/views.py
+++ app/merchant/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, abort
 from . import merchant
 from ..decorators import merchant_required
 from flask.ext.login import login_required
@@ -9,3 +9,13 @@ from flask.ext.login import login_required
 @merchant_required
 def index():
     return render_template('main/index.html')
+
+
+@merchant.route('/items/<int:listing_id>')
+@merchant.route('/items/<int:listing_id>/info')
+@login_required
+@merchant_required
+def listing_info(listing_id):
+    """View a listing's info."""
+    """TODO: Create listing's info view for merchants"""
+    abort(404)
