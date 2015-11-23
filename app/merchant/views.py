bc2a52f7ddd14c11b4cdc1ca1f18376a2512abc8
Cathy Chen <cathychenn@gmail.com>
2015-11-23 05:24:19 -0500

    fixed merge weirdness

--- app/merchant/views.py
+++ app/merchant/views.py
@@ -1,10 +1,14 @@
-from flask import render_template, abort, request, redirect, url_for, jsonify
+from flask import render_template, abort, request, redirect, url_for
 from . import merchant
 from ..decorators import merchant_required
 from flask.ext.login import login_required, current_user
-from ..models import Listing, CartItem, Order
+from ..models import Listing, CartItem
 from .. import db
-from forms import CartQuantityForm
+# from forms import SearchForm
+
+from forms import (
+    CartQuantityForm
+)
 
 
 @merchant.route('/')
@@ -19,22 +23,21 @@ def index():
 @merchant_required
 def listing_view_all():
     """Search for listings"""
+    # page = request.args.get('page', 1, type=int)
     main_search_term = request.args.get('main-search', "", type=str)
     favorite = True if request.args.get('favorite') == "on" else False
     name_search_term = request.args.get('name-search', "", type=str)
-    min_price = request.args.get('min-price', "", type=float)
-    max_price = request.args.get('max-price', "", type=float)
     listings = Listing.search(available=True,
                               favorite=favorite,
-                              min_price=min_price,
-                              max_price=max_price,
                               name_search_term=name_search_term,
                               main_search_term=main_search_term)
+    # pagination = Listing.query.paginate(
+    #         page, per_page=20,
+    #         error_out=False)
+    # listings = pagination.items
     return render_template('merchant/view_listings.html',
                            listings=listings,
                            main_search_term=main_search_term,
-                           min_price=min_price,
-                           max_price=max_price,
                            name_search_term=name_search_term,
                            favorite=favorite,
                            cart_listings=current_user.get_cart_listings(),
@@ -45,26 +48,15 @@ def listing_view_all():
 @login_required
 @merchant_required
 def cart_action():
-    def save_cart():
+    if request.form['submit'] == "Save Cart":
         for item in current_user.cart_items:
-            qty = int(request.form[str(item.listing_id)])
-            if qty == 0:
-                db.session.delete(item)
-            else:
-                item.quantity = qty
+            item.quantity = request.form[str(item.listing_id)]
         db.session.commit()
 
-    if request.form['submit'] == "Save Cart":
-        save_cart()
         return redirect(url_for('.manage_cart'))
 
     elif request.form['submit'] == "Order Items":
-        save_cart()
-        order = Order(current_user.cart_items)
-        db.session.add(order)
-        CartItem.delete_cart_items()
-        db.session.commit()
-        return redirect(url_for('.manage_cart'))
+        pass  # order items (dubin)
 
 
 @merchant.route('/manage-cart')
@@ -72,6 +64,7 @@ def cart_action():
 @merchant_required
 def manage_cart():
     form = CartQuantityForm()
+    print "HELLOOOOOOO?"
     return render_template('merchant/manage_cart.html',
                            cart=current_user.cart_items,
                            form=form)
@@ -87,51 +80,57 @@ def listing_info(listing_id):
     abort(404)
 
 
-@merchant.route('/change_in_cart/<int:listing_id>', methods=["PUT"])
+@merchant.route('/view/search/<string:search>')
 @login_required
 @merchant_required
-def change_in_cart(listing_id):
-    listing = Listing.query.filter_by(id=listing_id).first()
-    if not listing:
+def listing_search(search):
+    """Search for listings"""
+    listings = Listing.search(term=search)
+    return render_template('merchant/view_listings.html',
+                           listings=listings,
+                           header="Search results for \"{}\"".format(search))
+
+
+@merchant.route('/add_to_cart')
+@login_required
+@merchant_required
+def add_to_cart():
+    current_listing_id = request.args.get('current_listing_id')
+    quantity_needed = request.args.get('quantity_needed')
+    if not current_listing_id or not quantity_needed:
+        return redirect(url_for('.index'))
+    listing = Listing.query.filter_by(id=current_listing_id).first()
+    cart_item = CartItem.query.filter_by(
+        merchant_id=current_user.id).filter_by(
+        listing_id=current_listing_id).first()
+
+    if listing is None:
         abort(404)
-    if not request.json:
-        abort(400)
-    if 'inCart' in request.json and type(request.json['inCart']) is not bool:
-        abort(400)
-    if 'quantity' in request.json and type(request.json['quantity']) is not int:
-        abort(400)
-    cart_item = CartItem.query.filter_by(merchant_id=current_user.id,
-                                         listing_id=listing_id).first()
-    quantity = request.json['quantity']
-    current_status = cart_item is not None
-    new_status = request.json.get('inCart', current_status)
-    if new_status and not current_status:
-        db.session.add(CartItem(merchant_id=current_user.id,
-                                listing_id=listing_id,
-                                quantity=quantity))
-    elif not new_status and current_status:
-        print cart_item
+
+    if cart_item is None:
+        db.session.add(CartItem(
+            merchant_id=current_user.id,
+            listing_id=current_listing_id,
+            quantity=quantity_needed
+        ))
+    else:
         db.session.delete(cart_item)
+
     db.session.commit()
-    return jsonify({'inCart': new_status, 'quantity': quantity})
+    return redirect(url_for('.listing_view_all'))
 
 
-@merchant.route('/change_favorite/<int:listing_id>', methods=['PUT'])
+@merchant.route('/add_to_favorites')
 @login_required
 @merchant_required
-def change_favorite(listing_id):
-    listing = Listing.query.filter_by(id=listing_id).first()
-    if not listing:
+def favorite():
+    listing_id = request.args.get('listing_id')
+    if not listing_id:
         abort(404)
-    if not request.json:
-        abort(400)
-    if 'isFavorite' in request.json and type(request.json['isFavorite']) is not bool:
-        abort(400)
-    old_status = listing in current_user.bookmarks
-    new_status = request.json.get('isFavorite', old_status)
-    if new_status and listing not in current_user.bookmarks:
-        current_user.bookmarks.append(listing)
-    elif not new_status and listing in current_user.bookmarks:
+    listing = Listing.query.filter_by(id=listing_id).first_or_404()
+    if listing in current_user.bookmarks:
         current_user.bookmarks.remove(listing)
+    else:
+        current_user.bookmarks.append(listing)
     db.session.commit()
-    return jsonify({'isFavorite': listing in current_user.bookmarks})
+    return redirect(url_for('.listing_view_all'))
