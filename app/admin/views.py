e5f56e4872ee25003f0c00f3e62da93212cd408f
Cathy Chen <cathychenn@gmail.com>
2015-12-11 01:12:43 -0500

    return to dashboard button

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,6 +1,6 @@
 from ..decorators import admin_required
 
-from flask import render_template, abort, redirect, flash, url_for
+from flask import render_template, abort, redirect, request, flash, url_for
 from flask.ext.login import login_required, current_user
 
 from forms import (
@@ -10,7 +10,7 @@ from forms import (
     NewCategoryForm
 )
 from . import admin
-from ..models import User, Role, Vendor, Merchant, Category
+from ..models import User, Role, Vendor, Merchant, Category, Listing
 from .. import db
 from ..email import send_email
 
@@ -21,6 +21,34 @@ from ..email import send_email
 def index():
     """Admin dashboard page."""
     return render_template('admin/index.html')
+
+
+@admin.route('/view/all')
+@login_required
+@admin_required
+def listing_view_all():
+    """Search for listings"""
+    main_search_term = request.args.get('main-search', "", type=str)
+    favorite = True if request.args.get('favorite') == "on" else False
+    name_search_term = request.args.get('name-search', "", type=str)
+    min_price = request.args.get('min-price', "", type=float)
+    max_price = request.args.get('max-price', "", type=float)
+    listings = Listing.search(available=True,
+                              favorite=favorite,
+                              min_price=min_price,
+                              max_price=max_price,
+                              name_search_term=name_search_term,
+                              main_search_term=main_search_term)
+
+    return render_template('merchant/view_listings.html',
+                           listings=listings,
+                           main_search_term=main_search_term,
+                           min_price=min_price,
+                           max_price=max_price,
+                           name_search_term=name_search_term,
+                           favorite=favorite,
+                           cart_listings=None,
+                           header="All listings")
 
 
 @admin.route('/view-categories')
