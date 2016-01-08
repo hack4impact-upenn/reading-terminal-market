c996daf8696929a512e90f89f4b3115bf0bef5fd
Jonathan Dubin <mail@jondubin.com>
2016-01-08 14:11:09 -0500

    fixed styling for order view tables and disabled order cancellations

--- app/order/views.py
+++ app/order/views.py
@@ -5,19 +5,19 @@ from ..models import Order, Status
 from flask import abort, jsonify
 from ..decorators import merchant_required, vendor_required
 
-@order.route('/cancel/<int:order_id>', methods=['PUT'])
-@login_required
-@merchant_required
-def cancel_order(order_id):
-    order = Order.query.get(order_id)
-    if not order or order.merchant_id != current_user.id:
-        abort(404)
-    if order.status != Status.PENDING:
-        abort(400)
-    order.status = Status.CANCELED
-    db.session.commit()
-    # TODO send emails
-    return jsonify({'status': 'canceled'})
+# @order.route('/cancel/<int:order_id>', methods=['DELETE'])
+# @login_required
+# @merchant_required
+# def cancel_order(order_id):
+#     order = Order.query.get(order_id)
+#     if not order or order.merchant_id != current_user.id:
+#         abort(404)
+#     if order.status != Status.PENDING:
+#         abort(400)
+#     order.status = Status.CANCELED
+#     db.session.commit()
+#     # TODO send emails
+#     return jsonify({'status': 'canceled'})
 
 
 @order.route('/approve/<int:order_id>', methods=['PUT'])
