502ab2cff812d39039aeca772138b48ec635ce5a
anniemeng <anniezmeng@gmail.com>
2016-02-28 09:32:18 -0500

    Removed id from ratings model

--- app/models/ratings.py
+++ app/models/ratings.py
@@ -4,7 +4,6 @@ from .. import db
 class Ratings(db.Model):
     __tablename__ = 'ratings'
 
-    id = db.Column(db.Integer, primary_key=True)
     vendor_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     merchant_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     star_rating = db.Column(db.Integer)
