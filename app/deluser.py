a23e81db1abfa39ee415907214949d01ad0aafd2
Abhi <suriabhinav1997@gmail.com>
2016-10-31 06:47:50 -0400

    debugging statement

--- app/deluser.py
+++ app/deluser.py
@@ -6,6 +6,8 @@ from models import User
 def del_user(user_id):
     app = create_app(os.getenv('FLASK_CONFIG') or 'default')
     with app.app_context():
-       user = User.query.filter_by(id=user_id).first()
-       db.session.delete(user)
-       db.session.commit()
+        user = User.query.filter_by(id=user_id).first()
+        print "user found !!!!!!!!" 
+        db.session.delete(user)
+        db.session.commit()
+        print "user deleted !!!!!!!!" 
